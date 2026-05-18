ddd98dcf0e37431791d86d7fe9ebe2993897eb63
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-05-18 14:31:15 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");//codespace
-        System.out.println("Hello World2");//vscode local pc 
-        System.out.println("Hello World2");//
+        System.out.println("Hello World2");//vscode local pc
     }
 }
