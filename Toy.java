8456128f9205dd42a256912575419d3f96e22d2b
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-05-25 13:34:36 +0000

    commit&

--- Toy.java
+++ Toy.java
@@ -1,21 +1,45 @@
-package chapter1;
 /**
  * Represents a toy item in the inventory
  * <p>
  * Each toy has a name, brand, price, quality in stock, 
  * and a size classufucation.
  * </p>
- * 
+ * <ul>
+ *      <li><b>name</b> - Name of the toy (e.g., "Gundam")</li>
+ *      <li><b>brand</b> - Manufacturer or brand (e.g, "Lego")</li>
+ *      <li><b>price</b> - Selling price of the toy</li>
+ *      <li><b>quantity</b> - Number of units available</li>
+ *      <li><b>size</b> - Size category (e.g., 'S','M','L')</li>
+ *  </ul>
+ * @author Marc Yim
+ * @version 1.0
+ * @since 1.0
  */
+
+//javadoc Toy.java - to generate javadoc html files
 public class Toy { 
-    String name;
-    String brand;
+    /** Name of the toy */
+    public String name;
+
+    /** Manufacturer or brand */
+    public String brand;
+
+    /** Selling price of the Toy */
     double price;
+
+    /** Number of units available */
     int quantity;
+
+    /** Size category (e.g., 'S','M','L') */
     char size;
-    
+    /**
+     * Set the toy object price;
+     * 
+     * @param price the new price of the toy
+     */
     public void setPrice(double price) {
         this.price = price;
+        Fruit4 f4;
     }
 
     public static void main(String[] args) {
