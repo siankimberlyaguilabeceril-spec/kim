187c252c8c7600d66ae1c5b8a71fc70a20bcb0c8
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-05-25 13:52:53 +0000

    kkkk

---
+++ Chapter 1/Main.java
@@ -0,0 +1,9 @@
+package Chapter 1;
+
+import java.util.*; // built-in classes
+import otherFolder.a.Makahiya; // from other folder
+import otherFolder.SunFlower;
+
+public class Main {
+    
+}
---
+++ folder/sunflower.java
@@ -0,0 +1,12 @@
+package folder;
+
+import otherFolder.a.A;
+
+public class sunflower {
+   public static void main(String[] args) {
+    A a = new A();
+        System.out.println(a.secretCode);
+}
+
+
+}
