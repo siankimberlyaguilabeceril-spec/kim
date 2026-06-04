57531254e65c7d4d1cc6263ef6efb53a6d91847b
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-06-04 08:12:41 +0000

    TASK 2

---
+++ Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+public static void main(String[] args) {
+
+byte zero = 0;
+short three = 3;
+int one = 1;
+float two = 2.0f;
+char h = 'H';
+boolean truth = true;
+
+String output = "" + h + three + one + one + zero + " w" + zero + "rld " + two + " " + truth;
+
+System.out.println(output);
+}
+    
+}
