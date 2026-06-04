442b1fee4b22f663952e11ad2484dbbd2148691a
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-06-04 09:03:14 +0000

    TASK 4

---
+++ Task4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Task4 {
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        if (input.equals(reversed)) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+    }
+}
