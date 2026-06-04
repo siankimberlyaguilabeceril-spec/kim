3098f98b15a3ee715a3e869c102a3cdc06b392ab
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-06-04 08:42:50 +0000

    RASK 3

---
+++ Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+    public static void main(String[] args) {
+    String a = new String("Hello");
+    String b = "Hello";
+    String c = "World";
+    String d = "Hello!";
+
+    boolean b1 = a.equals(b); 
+    boolean  b2 = d.equals(b + "!");
+    boolean b3 = !c.equals(a);
+
+   if (b1 && b2 && b3) { 
+    System.out.println("Success!");
+   }
+
+}
+}
