4b0386b53d85dc065ec69e4cda87e261e2d43eb2
siankimberlyaguilabeceril-spec <siankimberlyaguilabeceril@gmail.com>
2026-06-26 10:29:23 +0000

    TASK 12

--- Task12.java
+++ Task12.java
@@ -17,7 +17,7 @@ class Point {
     }
 }
 
-public class Tasksheet12 {
+public class Task12 {
     public static void main(String[ ] args) {
         Point p = new Point(32, 32);
         for (int i = 0; i < 5; i++) {
